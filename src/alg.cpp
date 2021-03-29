2aaa5be3bf66321d2701a5658abc3d6a7277b666
MMTES <42440704+MMTES@users.noreply.github.com>
2021-03-29 13:53:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-	int r = size;
-	int m = 0;
-	int result = 0;
-	int l = 0;
-	while (true) {
+        int r = size;
+        int m = 0;
+        int result = 0;
+        int l = 0;
+        while (true) {
         m = (r + l)/2;
         if (value < arr[m]) {
-		r = m - 1;
-	}
-	else if (value > arr[m]) {
-		l = m + 1;
-	}
-            else {
-		    while (arr[m] == value) {
-			    m--;
-		 }
+        r = m - 1;
+        }
+        else if (value > arr[m]) 
+        {
+             l = m + 1;
+        }
+           else 
+           {
+              while (arr[m] == value) 
+              {
+              m--;
+              }
             m++;
-            while (arr[m] == value) {
-                result++;
-                m++;
-	    }
-            break;
-			}
+              while (arr[m] == value) 
+              {
+              result++;
+              m++;
+              }
+           break;
+           }
         if (l > r) {
-	break;
-	}
-	return result;
+        break;
+        }
+        return result;
+     }
 }
